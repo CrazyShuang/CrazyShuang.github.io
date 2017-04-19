7939f40c46b2615db91c4848f17f03004a5c8e28
crazyshuang <1802702500@qq.com>
2017-04-19 18:14:24 +0800

    Site updated: 2017-04-19 18:14:23

--- js/main.js
+++ js/main.js
@@ -141,4 +141,10 @@ require([], function (){
         $(".article a[href]").attr("target", "_blank")
     }
     $(".archive-article-title").attr("target", "_blank");
+
+    if($(".instagram").length) {
+        require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
+            obj.init();
+        });
+    }
 });
